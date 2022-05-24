ed914c00223a86d1bf778a087658eca7e8e30a51
mabarbar <mateusz.barzenc00@gmail.com>
2022-05-24 19:19:50 +0200

    add initial files

---
+++ index.js
@@ -0,0 +1,8 @@
+// alert("Jaka parówa wariacie?");
+console.log("'Witaj świecie' ~konsola")
+console.log("'Witaj świecie' ~konsola")
+
+const name = "Paulina"
+console.log(name)
+
+// const name = "Paulina"
