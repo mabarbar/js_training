f72364874c1e8390c1e3a660f9733256bca8889e
mabarbar <mateusz.barzenc00@gmail.com>
2022-05-25 22:47:57 +0200

    update repo

---
+++ js2/index.js
@@ -0,0 +1,8 @@
+const password = "3j1wew8ahsad"
+
+if (password.length > 10){
+    console.log("Masz hasło które ma więcej niż 10 znaków...");
+  } 
+else {
+    console.log("Nie masz hasła, które ma więcej niż 10 znaków...");
+  }
