3d7cd5d84290a6ff307e0cc8a2c33a26bb1a1ebf
mabarbar <mateusz.barzenc00@gmail.com>
2022-07-19 19:39:20 +0200

    update

--- js6_1/index.js
+++ js6_1/index.js
@@ -1,15 +1,28 @@
-const btn = document.querySelector("button");
-console.log(btn.outerHTML);
-console.log(btn.innerHTML);
+// const btn = document.querySelector("button");
+// console.log(btn.outerHTML);
+// console.log(btn.innerHTML);
 
 // btn.outerHTML = "123";
 // btn.innerHTML = "123";
 
-btn.innerHTML = "<li>siup</li>"
+// btn.innerHTML = "<li>siup</li>"
 // document.body.innerHTML = ""
 
-console.log(btn.innerText);
-console.log(btn.textContent);
+// console.log(btn.innerText);
+// console.log(btn.textContent);
 
-btn.textContent = "abcd"
-btn.innerText = "<li>abcd</li>"
+// btn.textContent = "abcd"
+// btn.innerText = "<li>abcd</li>"
+
+// const innerT = document.querySelector(".inner-text")
+// const textC = document.querySelector(".text-content")
+// console.log(innerT.innerText);
+// console.log(textC.textContent);
+
+const blockElement = document.querySelector("div");
+const butttton = document.querySelector("button");
+
+// blockElement.removeChild(butttton)
+// document.body.removeChild(blockElement)
+
+blockElement.remove();
