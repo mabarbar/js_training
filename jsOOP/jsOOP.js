f0fa82d2a8778cf80f0c10a8f7b574082f452d6e
mabarbar <mateusz.barzenc00@gmail.com>
2022-08-10 22:44:31 +0200

    update

--- jsOOP/jsOOP.js
+++ jsOOP/jsOOP.js
@@ -1,39 +1,55 @@
-const user = {
-  name: "Mateusz",
-  age: "22",
-  car: {
-    brand: "Nissan",
-    model: "Micra",
-    // sound: () => {
-    //   console.log("brrum");
-    // }
-    sound() {
-      console.log("brrum");
-    },
-  },
-};
+// const user = {
+//   name: "Mateusz",
+//   age: "22",
+//   car: {
+//     brand: "Nissan",
+//     model: "Micra",
+//     // sound: () => {
+//     //   console.log("brrum");
+//     // }
+//     sound() {
+//       console.log("brrum");
+//     },
+//   },
+// };
 
-user.hair = "brunette";
-user["fav-color"] = "white";
+// user.hair = "brunette";
+// user["fav-color"] = "white";
 
-user.car.sound();
-console.log(user);
+// user.car.sound();
+// console.log(user);
 
+// const dogAge = 7;
+// const dogBreed = "York";
+// const dogName = "Holi";
 
-const dogAge = 7;
-const dogBreed = "York";
-const dogName = "Holi";
+// const dog = {
+//   dogAge: dogAge,
+//   breed: dogBreed,
+//   dogName,
+// };
 
-const dog = {
-  dogAge: dogAge,
-  breed: dogBreed,
-  dogName,
-};
+// dog["dog-food"] = "Royal Canin";
 
-dog["dog-food"] = "Royal Canin"
+// console.log(dog);
 
-console.log(dog);
+// for (data in dog) {
+//   console.log(`Key: ${data}, Value: ${dog[data]}`);
+// }
 
-for(data in dog) {
-  console.log(`Key: ${data}, Value: ${dog[data]}`);
+// const user = {
+//   name: "Mateusz",
+//   printName() {
+//     console.log(this.name);
+//   }
+// };
+
+// user.printName()
+
+function User(name, age) {
+  this.name = name;
+  this.age = age;
 }
+
+const newUser = new User("Paulina", 22);
+console.log(newUser);
