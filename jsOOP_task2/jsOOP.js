30a4ff088e195d1a60cdeceb6e420cde693fd098
mabarbar <mateusz.barzenc00@gmail.com>
2022-08-12 21:34:56 +0200

    update

--- jsOOP_task2/jsOOP.js
+++ jsOOP_task2/jsOOP.js
@@ -1,8 +1,10 @@
 const btns = document.querySelectorAll("button");
 
-btns.forEach(btn => btn.addEventListener("click", function() {
-  console.log(this.textContent);
-}))
+btns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    console.log(this.textContent);
+  })
+);
 
 function test() {
   console.log(this.name);
@@ -10,14 +12,30 @@ function test() {
 
 const car1 = {
   name: "Nissan",
-}
+};
 
 const car2 = {
   name: "KIA",
-}
+};
 
 const car3 = {
   name: "Toyota",
-}
+};
 
-test.bind(car1)()
+test.bind(car1)();
+
+
+
+// const movie = {
+//   title: "Adwokat diabła",
+// };
+
+// function showMovie(price, cinema) {
+//   console.log(`Film ${this.title}, cena: ${price}zł, kino: ${cinema}. `);
+// }
+
+// const movieData = [20, "Multikino"];
+
+// showMovie(20, "Cinemacity");
+// showMovie.call(movie, 20, "Cinemacity");
+// showMovie.apply(movie, movieData);
