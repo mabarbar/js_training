a92c1eaf40f521f68036d2352d5be8cddb8637ed
mabarbar <mateusz.barzenc00@gmail.com>
2022-08-14 23:38:22 +0200

    update

--- js7/index.js
+++ js7/index.js
@@ -2,19 +2,20 @@ const btn = document.querySelector("button");
 const img = document.querySelector("img");
 
 const URL = "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true";
-
-btn.addEventListener("click", () => {
-  fetch(URL)
-    .then((res) => res.json())
-    .then((res) => img.setAttribute("src", res))
-    .catch((err) => console.log(err));
-});
+const URL2 = "https://api.waifu.pics/sfw/waifu";
 
 shibe = () => {
   fetch(URL)
     .then((res) => res.json())
     .then((res) => img.setAttribute("src", res))
-    .catch((err) => console.err(err));
+    .catch((err) => console.error(err));
 };
 
-shibe()
+btn.addEventListener("click", () => {
+  fetch(URL2)
+    .then((res) => res.json())
+    .then((data) => img.setAttribute("src", data.url))
+    .catch((err) => console.log(err));
+});
+
+shibe();
