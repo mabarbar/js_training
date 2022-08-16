70175d6a8130569771a0b023dd55b655510ae9a3
mabarbar <mateusz.barzenc00@gmail.com>
2022-08-17 00:00:52 +0200

    update

--- js9_1/index.js
+++ js9_1/index.js
@@ -6,3 +6,13 @@ const URL = "https://dog.ceo/api/breeds/image/random";
 fetch(URL)
   .then((res) => res.json())
   .then((res) => one.setAttribute("src", res.message));
+
+
+axios.get(URL).then(res => two.setAttribute("src", res.data.message))
+
+async function showImage() {
+  const response = await axios.post(URL)
+  three.setAttribute("src", response.data.message)
+}
+
+showImage()
